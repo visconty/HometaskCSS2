ffe5ccf731df6c706d0df2dd70dcc3e8ea2b6bf4
Vika Shestmintseva <v.shestmintseva@gmail.com>
2018-02-09 17:26:59 +0200

    Tempee

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+var day = prompt('What is the day today?');
+alert(day)
+
+// var message;
+// if (role == 'Гость') {
+//   message = 'Привет';
+// } else if (role == 'Директор') {
+//   message = 'Здравствуйте';
+// } else if (role == '') {
+//   message = 'Познакомимся?';
+// } else {
+//   message = '';
+// }
+// var role = 'Гость'?
+// var message = 'Привет'
